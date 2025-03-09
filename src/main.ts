a0beffa86baa3dbf86a240d3a98b418a04ec06c7
shaheerahmed99 <shaheer.99.ahmed@gmail.com>
2025-03-09 15:03:41 +0500

    change/main

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -15,7 +16,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3001, '0.0.0.0');
 }
 bootstrap();
