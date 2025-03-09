7a0d746b32d90ff5ce9e932ab47aa4a9729427ad
shaheerahmed99 <shaheer.99.ahmed@gmail.com>
2025-03-09 17:01:21 +0500

    fix: token issues

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,13 +7,16 @@ import { JwtModule } from '@nestjs/jwt';
 import { EmailService } from 'src/email/email.service';
 import { ConfigService } from '@nestjs/config';
 import { ConfigModule } from '@nestjs/config';
+import * as fs from 'fs';
+import * as path from 'path';
 
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     JwtModule.register({
-      secret: process.env.JWT_PRIVATE_KEY,
+      privateKey: fs.readFileSync(path.join(__dirname, '../../keys/private.pem')),
+      publicKey: fs.readFileSync(path.join(__dirname, '../../keys/public.pem')),
       signOptions: { algorithm: 'RS256', expiresIn: '1h' },
     }),
     ConfigModule.forRoot(),
--- src/database/database.ts
+++ src/database/database.ts
@@ -7,7 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       type: 'postgres',
       url: "postgres://neondb_owner:npg_i7aZCyJ4HsPh@ep-solitary-flower-a8h5b8fx-pooler.eastus2.azure.neon.tech/dax-ecommerce-db?sslmode=require",
       autoLoadEntities: true,
-      synchronize: true, // Set to `false` in production
+      synchronize: false,
       ssl: process.env.DATABASE_URL?.includes('neon.tech')
         ? { rejectUnauthorized: false } // ✅ Enable SSL for NeonDB
         : false,
